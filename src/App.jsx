d63325b3e04d02b88d36436b00d7814291a48007
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-26 17:34:42 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Message from './components/greeting'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <Message />
-    </>
+    <Router>
+    <Routes>
+      <Route path="/messages" element={<Message />} />
+    </Routes>
+  </Router>
   )
 }
 
